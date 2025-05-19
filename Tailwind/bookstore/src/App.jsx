68a032d1a283679c782951fd0ddf6cca65a0fbc7
HassanShamil <hassanshamil55@gmail.com>
2025-05-19 23:13:33 +0530

    basic react + tailwind css setup

---
+++ Tailwind/bookstore/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-blue-600 text-3xl font-bold">
+      Welcome to the bookstore application
+    </div>
+  );
+}
+
+export default App;
